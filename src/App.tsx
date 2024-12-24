ed176a5a5a9498dfd1a27297058cbff2e33a0268
jhorsax161616 <71660993@continental.edu.pe>
2024-12-24 15:45:39 -0500

    Using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
-import { Button } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('Jhorsax')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  
+  const fetchData = useCallback(
+    async () => {
+      setLoading(true)
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        if (!response.ok) {
+          throw new Error('Se produjo un error al solicitar los datos')
+        }
 
-  const countMore = () => {
-    setCount((count) => count + 1)
-  }
+        const jsonData = await response.json()
+        setData(jsonData)
+      } catch (err) {
+        setError(err as string)
+      } finally {
+        setLoading(false)
+      }
+    },
+    [loading],
+  )
 
-  const changeName = () => {
-    setName('Jhordan Sax')
-  }
+  // Comunicarnos con un endpoint
+  /* ¿Cuándo usamos un useEffect?
+  El uso correcto de useEffect es en los siguientes casos:
+  - Para sincronizar con entidades externas (APIs, bases de datos, etc.)
+  - Operaciones asíncronas
+  - Para parametros de entrada de un componente
+  */
+  useEffect(() => {
+    // Aquí se hace la petición, y siempre se ejecuta cuando el componente se monta o se actualiza el estado de data
+    fetchData()
+  }, [fetchData])
+
+  if (loading) return <h1>Loading...</h1>
+
+  if (error) return <h1>Ups! Ocurrió un error: {error}</h1>
 
   return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} />
-      <p>{name}</p>
-      <Button label="Change Name" parentMethod={changeName} />
-    </>
+    <div className="App">
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
