c82bd2e631169fd428f658ae2293e3cc18ce6531
jhorsax161616 <71660993@continental.edu.pe>
2024-12-26 15:09:51 -0500

    Composition Pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import './App.css'
-import { useFetch } from './hooks'
-
-const url = 'https://jsonplaceholder.typicode.com/posts'
-
-interface Data {
-  userId: number
-  id: number
-  title: string
-  body: string
-}
+import { Button, ColorRed } from './components'
 
 function App() {
-  const {data, error, loading} = useFetch<Data[]>(url)
+  const handleClick = () => {
+    alert('Button clicked')
+  }
 
-  if (loading) return <h1>Loading...</h1>
-
-  if (error) return <h1>Ups! Ocurrió un error: {error.message}</h1>
-
+  const sayHello = () => {
+    alert('Hello!!!')
+  }
+  
   return (
-    <div className="App">
-      <h1>Posts</h1>
-      <ul>
-        {data?.map((post: any) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <ColorRed>
+        <Button parentMethod={sayHello}>
+          Click me
+        </Button>
+      </ColorRed>
+      <Button parentMethod={handleClick}>
+        Click me
+      </Button>
+    </>
   )
 }
 
